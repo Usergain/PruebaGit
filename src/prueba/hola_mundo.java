04055f0869a6ffa71127a2b08e0c9db38e0847a1
usergain <usergain@hotmail.com>
2021-12-27 11:42:48 +0100

    parrafo y tamaño fuente

--- src/prueba/hola_mundo.java
+++ src/prueba/hola_mundo.java
@@ -5,7 +5,8 @@ public class hola_mundo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola mundo");
-
+		System.out.println("1,2,3 HACIENDO PRUEBAS");
+		System.out.println("seguimos con los commits");
 	}
 
 }
